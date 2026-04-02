0eedede8b83ec92ddae2429039cf9493d2667565
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-04-02 12:34:31 +0700

    fff

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = "https://e45a-58-8-174-156.ngrok-free.app"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://321a-49-0-82-165.ngrok-free.app
+export const apiUrl = "https://c21f-58-8-174-156.ngrok-free.app"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://321a-49-0-82-165.ngrok-free.app
 
 export const defaultHeaders = {
   "Content-Type": "application/json",
