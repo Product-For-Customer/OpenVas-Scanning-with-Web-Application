faf7754b518790057deda8369a200a05f8e28292
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-04-02 11:24:04 +0700

    add

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = "https://postdiphtherial-unperishable-carolyn.ngrok-free.dev"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://321a-49-0-82-165.ngrok-free.app
+export const apiUrl = "https://321a-49-0-82-165.ngrok-free.app"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://321a-49-0-82-165.ngrok-free.app
 
 export const defaultHeaders = {
   "Content-Type": "application/json",
