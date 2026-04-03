545d5db661d024dbafa97fb8191f05c91144aeb6
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-04-03 10:01:29 +0700

    add

--- frontend/src/page/dashboard/index.tsx
+++ frontend/src/page/dashboard/index.tsx
@@ -7,25 +7,25 @@ import TopVulnerability from "./Vulnerbility/index";
 
 const DashboardIndex: React.FC = () => {
   return (
-    <div className="w-full overflow-visible">
-      <div className="relative z-60 mb-4 sm:mb-5">
+    <div className="relative z-0 w-full overflow-visible">
+      <div className="relative z-0 mb-4 sm:mb-5">
         <Value />
       </div>
 
-      <div className="relative z-10 mb-4 sm:mb-5">
+      <div className="relative z-0 mb-4 sm:mb-5">
         <AverageEnrollment />
       </div>
 
       <div className="relative z-0 mb-4 grid grid-cols-1 gap-4 sm:gap-5 xl:grid-cols-12 xl:auto-rows-fr xl:items-stretch">
-        <div className="xl:col-span-4 flex w-full h-full min-h-0">
+        <div className="flex h-full min-h-0 w-full xl:col-span-4">
           <TopVulnerability />
         </div>
 
-        <div className="xl:col-span-4 flex w-full h-full min-h-0">
+        <div className="flex h-full min-h-0 w-full xl:col-span-4">
           <DeliveryAnalysis />
         </div>
 
-        <div className="xl:col-span-4 flex w-full h-full min-h-0">
+        <div className="flex h-full min-h-0 w-full xl:col-span-4">
           <TopPerforming />
         </div>
       </div>
